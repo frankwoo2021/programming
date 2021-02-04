c8daf4bd99035dde5cbb9ae60a99b4781a72fc6f
frankwoo2021 <frankwoo2015@outlook.com>
2021-02-04 17:07:18 +0800

    commit2

--- go/mygo/src/proj_1/hello.go
+++ go/mygo/src/proj_1/hello.go
@@ -1,7 +1,19 @@
 package main 
 import (
 	"fmt"
+	"errors"
 )
 func main()  {
 	fmt.Println("hello go!")
+
+	a, b := 4, 0
+	val, err := testDiv(a,b)
+	fmt.Println(val, err)
+}
+
+func testDiv(a, b int) (int, error)  {
+	if b == 0 {
+		return 0, errors.New("divided by zero")
+	}
+	return a/b, nil
 }
