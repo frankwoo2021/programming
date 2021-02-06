72ad5e5b79816482056e8a79366a7bcb61d50003
frankwoo2021 <frankwoo2015@outlook.com>
2021-02-06 20:52:35 +0800

    commit go ex.

--- go/mygo/src/proj_1/hello.go
+++ go/mygo/src/proj_1/hello.go
@@ -9,6 +9,17 @@ func main()  {
 	a, b := 4, 0
 	val, err := testDiv(a,b)
 	fmt.Println(val, err)
+
+	f := testFunc(3)
+	f(4)
+
+	testDefer(10,2)
+
+	testSlice()
+
+	testMap()
+
+	testStruct()
 }
 
 func testDiv(a, b int) (int, error)  {
@@ -16,4 +27,50 @@ func testDiv(a, b int) (int, error)  {
 		return 0, errors.New("divided by zero")
 	}
 	return a/b, nil
+}
+
+func testFunc(x int) func (int)  {
+	return func (y int)  {
+		fmt.Println(x+y)
+	}
+}  
+
+func testDefer(a, b int)  {
+	defer fmt.Println("dispose ...")
+	fmt.Println(a/b)
+}
+
+func testSlice()  {
+	x := make([]int, 0, 5)
+	for i := 0; i < 8; i++ {
+		x = append(x, i)
+	}
+	fmt.Println(x)
+}
+
+func testMap()  {
+	m := make(map[string]int)
+	m["a"] = 1
+	x, ok := m["b"]
+	fmt.Println(x, ok)
+	x, ok = m["a"]
+	fmt.Println(x, ok)
+	delete(m, "a")
+}
+
+func testStruct()  {
+	type user struct {
+		name string
+		age byte
+	}
+	type manager struct {
+		user
+		title string
+	}
+	var m manager
+	m.name = "Tom"
+	m.age = 20
+	m.title = "CEO"
+
+	fmt.Println(m)
 }
