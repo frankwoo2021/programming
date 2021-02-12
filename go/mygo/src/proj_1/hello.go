6671cdbc8b0e294d0e54daf3b74abe9ef3f9b12b
frankwoo2021 <frankwoo2015@outlook.com>
2021-02-12 18:06:09 +0800

    somenew

--- go/mygo/src/proj_1/hello.go
+++ go/mygo/src/proj_1/hello.go
@@ -1,46 +1,55 @@
-package main 
+package main
+
 import (
-	"fmt"
 	"errors"
+	"fmt"
+	"time"
 )
-func main()  {
+
+func main() {
 	fmt.Println("hello go!")
 
 	a, b := 4, 0
-	val, err := testDiv(a,b)
+	val, err := testDiv(a, b)
 	fmt.Println(val, err)
 
 	f := testFunc(3)
 	f(4)
 
-	testDefer(10,2)
+	testDefer(10, 2)
 
 	testSlice()
 
 	testMap()
 
 	testStruct()
+
+	go testCur(1)
+	go testCur(2)
+	time.Sleep(time.Second * 6)
+
+	testCP()
 }
 
-func testDiv(a, b int) (int, error)  {
+func testDiv(a, b int) (int, error) {
 	if b == 0 {
 		return 0, errors.New("divided by zero")
 	}
-	return a/b, nil
+	return a / b, nil
 }
 
-func testFunc(x int) func (int)  {
-	return func (y int)  {
-		fmt.Println(x+y)
+func testFunc(x int) func(int) {
+	return func(y int) {
+		fmt.Println(x + y)
 	}
-}  
-
-func testDefer(a, b int)  {
-	defer fmt.Println("dispose ...")
-	fmt.Println(a/b)
 }
 
-func testSlice()  {
+func testDefer(a, b int) {
+	defer fmt.Println("dispose ...")
+	fmt.Println(a / b)
+}
+
+func testSlice() {
 	x := make([]int, 0, 5)
 	for i := 0; i < 8; i++ {
 		x = append(x, i)
@@ -48,9 +57,9 @@ func testSlice()  {
 	fmt.Println(x)
 }
 
-func testMap()  {
+func testMap() {
 	m := make(map[string]int)
-	m["a"] = 1
+	m["a"] = 5
 	x, ok := m["b"]
 	fmt.Println(x, ok)
 	x, ok = m["a"]
@@ -58,10 +67,10 @@ func testMap()  {
 	delete(m, "a")
 }
 
-func testStruct()  {
+func testStruct() {
 	type user struct {
 		name string
-		age byte
+		age  byte
 	}
 	type manager struct {
 		user
@@ -73,4 +82,32 @@ func testStruct()  {
 	m.title = "CEO"
 
 	fmt.Println(m)
+}
+
+func testCur(id int) {
+	for i := 0; i < 5; i++ {
+		fmt.Printf("this is %d: %d\n", id, i)
+		time.Sleep(time.Second)
+	}
+}
+
+func testCP() {
+	consumer := func(data chan int, done chan bool) {
+		for x := range data {
+			fmt.Println("recieve: ", x)
+		}
+		done <- true
+	}
+	producer := func(data chan int) {
+		for i := 0; i < 4; i++ {
+			data <- i
+		}
+		close(data)
+	}
+	done := make(chan bool)
+	data := make(chan int)
+
+	go consumer(data, done)
+	go producer(data)
+	<-done
 }
